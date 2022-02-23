36d291f8aad464a310a669b01e8b5dcc6fca54bc
arpitdalal <arpitdalalm@gmail.com>
2022-02-23 16:27:20 -0500

    update npm packages and convert index component from class to function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Loader from './Loader';
+
 import Error from './Error';
+import Loader from './Loader';
+import SeasonDisplay from './SeasonDisplay';
 
-class App extends React.Component {
-	// Defining state which basically is a js object
-	// and the properties in this obj can be modified using this.setState() and can be used as this.state.propertyName
-	// and when setState() is called React will rerender the app
-	state = {
-		lat: null,
-		errMsg: ''
-	};
+const App = () => {
+	const [lat, setLat] = useState();
+	const [errMsg, setErrMsg] = useState();
 
-	// Lifecycle method
-	componentDidMount() {
+	useEffect(() => {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => this.setState({ lat: position.coords.latitude }), // will not modify errMsg
-			(err) => this.setState({ errMsg: err.message }) // will not modify lat
+			position => {
+				setLat(position.coords.latitude);
+			},
+			err => {
+				setErrMsg(err.message);
+			}
 		);
-	}
+	}, [])
 
-	// Helper method to do all the logic
-	renderContent() {
-		if (this.state.errMsg && !this.state.lat) {
-			return <Error msg={this.state.errMsg} />;
-		} else if (!this.state.errMsg && this.state.lat) {
-			return <SeasonDisplay lat={this.state.lat} />;
-		} else {
-			return <Loader msg="Please accept location request :)" />;
-		}
-	}
-
-	// render() should only render content and no logic
-	render() {
-		return this.renderContent();
-	}
+	return (
+		<React.Fragment>
+			{
+				(errMsg && !lat) ? <Error msg={errMsg} /> :
+				(!errMsg && lat) ? <SeasonDisplay lat={lat} /> :
+				<Loader msg="Please accept location request :)" />
+			}
+		</React.Fragment>
+	)
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
